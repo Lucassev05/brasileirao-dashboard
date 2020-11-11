46676b993750860575be857a9dc64cf76a06021e
Lucas Evangelista <lucas.evangelista@cubos.io>
2020-11-10 21:22:43 -0300

    fix: header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,55 +1,68 @@
 import React from "react";
 import "./header.css";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+// const useStyles = withStyles((theme) => ({
+//   root: {
+//     "& > *": {
+//       margin: theme.spacing(1),
+//       width: "25ch",
+//       borderColor: "red",
+//     },
+//   },
+// }));
+
+const CssTextField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "white",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#808080",
+      },
+      "&:hover fieldset": {
+        borderColor: "white",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "white",
+      },
+    },
+    multilineColor: {
+      color: "red",
+    },
+  },
+})(TextField);
 
 const Header = (props) => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
+  //   const classes = useStyles();
   return (
-    <div className="menu">
-      <div className="conteudo">
-        <h1>Brasileirão</h1>
-        <div className="login">
-          {props.token == "" ? (
-            <div>
-              <label className="email">
-                Email
-                <input
-                  onChange={(event) => setEmail(event.target.value)}
-                  value={email}
-                  type="email"
-                />
-              </label>
-              <label className="senha">
-                Senha
-                <input
-                  onChange={(event) => setPassword(event.target.value)}
-                  value={password}
-                  type="password"
-                />
-              </label>
-              <button
-                onClick={() =>
-                  props.realizarLogin({
-                    email: email,
-                    password: password,
-                  })
-                }
-              >
-                Logar
-              </button>
-            </div>
-          ) : (
-            <button
-              onClick={() => {
-                props.setToken("");
-                setEmail("");
-                setPassword("");
-              }}
-            >
-              Deslogar
-            </button>
-          )}
+    <div>
+      {/* <div className="login">
+        <h1>Realize seu login para editar</h1>
+        <form noValidate autoComplete="off">
+          <CssTextField
+            id="outlined-basic"
+            label="E-mail"
+            type="email"
+            variant="outlined"
+          />
+          <CssTextField
+            className="input"
+            id="outlined-basic"
+            label="Senha"
+            type="password"
+            variant="outlined"
+          />
+        </form>
+      </div> */}
+      <div className="header">
+        <div className="content">
+          <h1>Brasileirão</h1>
+          <PowerSettingsNewIcon className="btn-logar" />
         </div>
       </div>
     </div>
